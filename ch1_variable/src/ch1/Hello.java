6933df697333ef4b48901c11092cbfaf769609d6
sunbeom <sunbum1234@naver.com>
2024-01-30 17:50:23 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello.java
@@ -0,0 +1,23 @@
+// 자바의 기본 단위 = > class
+/* ch1 => 패키지(관련있는 클래스들 모음)
+ * main => mathod : 이름()
+ * 
+ */
+ package ch1;
+
+public class Hello { //클래스 이름은 대문자로 시작(한글이 가능하나 영어 사용)
+    public static void main(String[] args) {
+        System.out.println(" 안녕하세요");
+        System.out.println(" 안녕하세요");
+        System.out.println(" 안녕하세요");
+        System.out.println(" 안녕하세요");
+
+    }
+}
+
+/* shif + alt + down key : 복사
+ * 
+ * 
+ * 
+ * 
+ */
